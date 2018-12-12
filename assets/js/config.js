1d850524c6bcd8be317134ccfc4f938503d10a52
songxingguo <songxingguo@xiao.ai>
2018-12-12 15:31:03 +0800

    feat:  锦鲤多多平台SEO项目修改配置信息为实际的信息

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,8 @@ const config = {
   goodCar: {
     title: '好车多多',
     subtitle: '和你一样喜欢好车的人，也在这里',
+    keywords: '汽车，买车，汽车网，汽车报价，汽车图片，新闻，评测，B级车',
+    description: '好车多多提供汽车资讯，精彩的汽车新闻、行情、评测、导购内容。',
     describe: '锦鲤多多，好运连连',
     pcUrl: '/images/goodCar-pc.jpg',
     mbUrl: '/images/goodCar-mb.jpg',
@@ -9,7 +11,9 @@ const config = {
   },
   goodDrink: {
     title: '好喝多多',
-    subtitle: '和你一样喜欢喝的人，也在这里',
+    subtitle: '和你一样喜欢好酒的人，也在这里',
+    keywords: '白酒，进口，葡萄酒，洋酒，红酒，啤酒，黄酒，小酒，老酒',
+    description: '好喝多多，干杯。',
     describe: '锦鲤多多，好运连连',
     pcUrl: '../images/goodDrink-pc.jpg',
     mbUrl: '../images/goodDrink-mb.jpg',
@@ -19,22 +23,28 @@ const config = {
     title: '好房多多',
     subtitle: '和你一样喜欢好房的人，也在这里',
     describe: '锦鲤多多，好运连连',
+    keywords: '二手房，租房，买房，房价，房地产，房产信息，笋盘',
+    description: '好房多多提供二手房租房买房等房产资讯。',
     pcUrl: '/images/goodHouse-pc.jpg',
     mbUrl: '/images/goodHouse-mb.jpg',
     QRCodeUrl: '/images/QR-code.png'
   },
   goodLooking: {
     title: '好看多多',
-    subtitle: '和你一样会欣赏的人，也在这里',
+    subtitle: '和你一样喜欢美的人，也在这里',
     describe: '锦鲤多多，好运连连',
+    keywords: '时尚，护肤，彩妆，美食，旅行，影视，读书，健身',
+    description: '好看多多，吃喝玩乐的日常。',
     pcUrl: '/images/goodLooking-pc.jpg',
     mbUrl: '/images/goodLooking-mb.jpg',
     QRCodeUrl: '/images/QR-code.png'
   },
   goodPlay: {
     title: '好玩多多',
-    subtitle: '和你一样会玩的人，也在这里',
+    subtitle: '和你一样喜欢玩的人，也在这里',
     describe: '锦鲤多多，好运连连',
+    keywords: '游戏，电视游戏，家用机游戏，单机游戏，游戏新闻， switch',
+    description: '好玩多多游戏，电视游戏，家用机游戏，单机游戏，游戏新闻， switch。',
     pcUrl: '/images/goodPlay-pc.jpg',
     mbUrl: '/images/goodPlay-mb.jpg',
     QRCodeUrl: '/images/QR-code.png'
