c411e2c6588144da9bccc3dc9908ef1e74dcc877
songxingguo <1328989942@qq.com>
2018-12-19 17:35:56 +0800

    feat: 为数据中的路径添加前缀

--- routes/release.js
+++ routes/release.js
@@ -1,21 +1,29 @@
-const { resolve, dirname, extname, join } = require('path')
-const pug = require('pug');
+const { resolve, join } = require('path')
+const { compileFile } = require('pug');
 const {mkdir, writeFileSync} = require('fs');
 
 const path = './../pages/index.pug';
 const data = require('./../assets/data/index.json');
 
-function translationMiddleware(path, { options } = {}) {
-  mkdir('./dist', function () {
+(function translateToHtml(path, {
+  options = {},
+  root = './dist/',
+  prefix = './../../assets',
+  fieldArr = ['pcUrl', 'mbUrl', 'QRCodeUrl']
+} = {}) {
+  // 创建根目录
+  mkdir(join(root), function () {
     for(const item in options) {
-      const compileFile = pug.compileFile;
+      // 为数据中的路径添加前缀
+      for (const field of fieldArr) {
+        options[item][field] = prefix + options[item][field];
+      }
+      // 将 pug 转义成 html
       const html =  compileFile(resolve(__dirname, path))(options[item]);
-
-      mkdir(join('./dist/', item), function () {
-        writeFileSync(join('./dist/', item, '/index.html'), html)
+      // 创建模块目录
+      mkdir(join(root, item), function () {
+        writeFileSync(join(root, item, '/index.html'), html)
       });
     }
-  });
-}
-
-translationMiddleware(path, {options: data});
+  })
+})(path, {options: data});
