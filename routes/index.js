32c7019712900bef498af11fb4f1a460bf4add92
xg.song <1328989942@qq.com>
2019-01-13 04:25:39 +0800

    改变 dev 的数据来源

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
 const router = require('koa-router')()
-const {goodCar, goodDrink, goodHouse, goodLooking, goodPlay} = require('../public/data/index.json')
+const data = require('../public/data/index.json')
 
 router.get('/', async (ctx, next) => {
-  const {host} = ctx.headers
-  const config = {
-    'www.haochedd.com': goodCar,
-    'www.haohedd.com': goodDrink,
-    'www.haofangdd.com': goodHouse,
-    'www.haokandd.com': goodLooking,
-    'www.haowandd.com': goodPlay
-  }[host] || goodCar
-  await ctx.render('index', config)
+  await ctx.render('index', data)
 })
 
 module.exports = router
